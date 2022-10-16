9a5eae44b8047ba71fcd5d17f9a5e226dd0db3e4
Gabriel Cristian <gabriel@megaconecta.com.br>
2022-10-15 23:33:44 -0300

    avançando nas aulas
    while

---
+++ aula8/interação.py
@@ -0,0 +1,17 @@
+#       Índices
+#       0123456...........................33
+frase = 'o rato roeu a roupa do rei de roma'  #Iteravel
+tamanho_frase = len(frase)
+contador = 0
+nova_string = ''
+
+# Iteração <- Iterar
+while contador < tamanho_frase:
+    letra = frase[contador]
+    if letra == 'r':
+        nova_string += 'R'
+    else:
+        nova_string += letra
+    contador += 1
+
+print(nova_string)
