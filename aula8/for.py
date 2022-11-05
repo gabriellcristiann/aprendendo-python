d46a1b023b420e96daa6978a5b72633c2064da80
Gabriel Cristian <gabriel@megaconecta.com.br>
2022-11-05 01:24:12 -0300

    Aula FOR

---
+++ aula8/for.py
@@ -0,0 +1,15 @@
+"""
+Split, Join, Enumerate em Python
+* Split - Dividir uma string
+* Join - juntar uma lista # str
+* Enumerate - Enumerar elementos da lista # iteraveis
+"""
+
+lista = [
+    [0,'Gabriel'],
+    [1,'Cristian'],
+    [2,'Cunha']
+]
+
+for indice, nome in lista:
+    print(indice, nome)
