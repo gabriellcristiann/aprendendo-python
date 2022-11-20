8af68cabae61b39ab34c5b4d55aa3b0502ee0065
Gabriel Cristian <gabriel@megaconecta.com,br>
2022-11-20 18:40:33 -0300

    ADICIONANDO AULA 09

---
+++ aula9/desempacotamento.py
@@ -0,0 +1,9 @@
+"""
+    Desempacotamento de listas em Python
+"""
+
+lista = ['Gabriel', 'Cristian', 'Cunha']
+
+n1, n2, n3 = lista
+
+print(n2)
---
+++ aula9/operador_ternario.py
@@ -0,0 +1,8 @@
+"""
+Operador ternário em Python
+"""
+
+logged_user = True
+msg = 'Usuario Logado.' if logged_user else 'Usuario Precisa Logar'
+
+print(msg)
